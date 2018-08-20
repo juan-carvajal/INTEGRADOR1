ced72f34f2a9042abee6d047b0556e57c91872e0
German Andres Carvajal Hernandez <german.carvajal@correo.icesi.edu.co>
2018-08-20 12:47:21 -0500

    Agregue mapa

--- AppMaps/AppMaps/Form1.Designer.cs
+++ AppMaps/AppMaps/Form1.Designer.cs
@@ -32,7 +32,9 @@
             this.TabMapa = new System.Windows.Forms.TabPage();
             this.Buscador = new System.Windows.Forms.TabPage();
             this.Reportes = new System.Windows.Forms.TabPage();
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
             this.tabControl1.SuspendLayout();
+            this.TabMapa.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -48,6 +50,7 @@
             // 
             // TabMapa
             // 
+            this.TabMapa.Controls.Add(this.gmap);
             this.TabMapa.Location = new System.Drawing.Point(4, 22);
             this.TabMapa.Name = "TabMapa";
             this.TabMapa.Padding = new System.Windows.Forms.Padding(3);
@@ -55,13 +58,14 @@
             this.TabMapa.TabIndex = 0;
             this.TabMapa.Text = "Mapa";
             this.TabMapa.UseVisualStyleBackColor = true;
+            this.TabMapa.Click += new System.EventHandler(this.TabMapa_Click);
             // 
             // Buscador
             // 
             this.Buscador.Location = new System.Drawing.Point(4, 22);
             this.Buscador.Name = "Buscador";
             this.Buscador.Padding = new System.Windows.Forms.Padding(3);
-            this.Buscador.Size = new System.Drawing.Size(252, 211);
+            this.Buscador.Size = new System.Drawing.Size(687, 346);
             this.Buscador.TabIndex = 1;
             this.Buscador.Text = "Navegar";
             this.Buscador.UseVisualStyleBackColor = true;
@@ -70,10 +74,38 @@
             // 
             this.Reportes.Location = new System.Drawing.Point(4, 22);
             this.Reportes.Name = "Reportes";
-            this.Reportes.Size = new System.Drawing.Size(252, 211);
+            this.Reportes.Size = new System.Drawing.Size(687, 346);
             this.Reportes.TabIndex = 2;
             this.Reportes.Text = "Reportes";
             this.Reportes.UseVisualStyleBackColor = true;
+            // 
+            // gmap
+            // 
+            this.gmap.Bearing = 0F;
+            this.gmap.CanDragMap = true;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gmap.LevelsKeepInMemmory = 5;
+            this.gmap.Location = new System.Drawing.Point(0, 0);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 2;
+            this.gmap.MinZoom = 2;
+            this.gmap.MouseWheelZoomEnabled = true;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(687, 346);
+            this.gmap.TabIndex = 0;
+            this.gmap.Zoom = 0D;
+            this.gmap.Load += new System.EventHandler(this.gMapControl1_Load);
             // 
             // AppGMaps
             // 
@@ -84,6 +116,7 @@
             this.Name = "AppGMaps";
             this.Text = "Grupos de Investigacion";
             this.tabControl1.ResumeLayout(false);
+            this.TabMapa.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -94,6 +127,7 @@
         private System.Windows.Forms.TabPage TabMapa;
         private System.Windows.Forms.TabPage Buscador;
         private System.Windows.Forms.TabPage Reportes;
+        private GMap.NET.WindowsForms.GMapControl gmap;
     }
 }
 
