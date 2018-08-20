20f9989b434138f0972ed19d4b0d59927cd7875b
JUAN DAVID <JUAN DAVID@JUAN>
2018-08-20 12:30:14 -0500

    Se agrego el modelo.

---
+++ AppMaps/AppMaps/modelo/Colciencias.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMaps.modelo
+{
+    class Colciencias
+    {
+
+        public List<GrupoInvestigacion> grupos;
+
+
+
+        public Colciencias()
+        {
+            grupos = new List<GrupoInvestigacion>();
+        }
+    }
+}
