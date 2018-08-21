85b45dc8beb0d83118f15d4c4895a8d1ccf2f32d
JUAN DAVID <JUAN DAVID@JUAN>
2018-08-20 21:32:42 -0500

    Se corrigo error al generar el mapa sin seleccionar ninguna opcion.

--- AppMaps/AppMaps/Form1.cs
+++ AppMaps/AppMaps/Form1.cs
@@ -372,6 +372,9 @@ namespace AppMaps
 
         private void button2_Click(object sender, EventArgs e)
         {
+            try
+            {
+
             String parS=listView2.SelectedItems[0].Text;
             int p1 = Int32.Parse(parS.Split('-')[0]);
             int p2 = Int32.Parse(parS.Split('-')[1]);
@@ -379,6 +382,11 @@ namespace AppMaps
 
 
             cambiarColorArticulos(par);
+            }
+            catch
+            {
+                MessageBox.Show("Se debe seleccionar una opcion.");
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
